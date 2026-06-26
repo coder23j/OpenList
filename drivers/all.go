bc538aa948e7387d81d302e512dde92281f18c20
coder23j <183291631+coder23j@users.noreply.github.com>
2026-06-26 16:58:45 +0800

    feat: add pCloud driver support

--- drivers/all.go
+++ drivers/all.go
@@ -61,6 +61,7 @@ import (
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/onedrive_sharelink"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/openlist"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/openlist_share"
+	_ "github.com/OpenListTeam/OpenList/v4/drivers/pcloud"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/pikpak"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/pikpak_share"
 	_ "github.com/OpenListTeam/OpenList/v4/drivers/proton_drive"
